bc1980a9d65f78d696d2c60beabde40709c15b1a
carlos <carlos94_cr7@hotmail.com>
2023-11-19 15:22:57 -0600

    se modifica tamaño de botón

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -13,13 +13,13 @@ export default function TodoApp() {
 
   function onUpdate(id, value) {
     const temp = [...todo];
-    const item = temp.find((e) => e.id == id);
+    const item = temp.find((e) => e.id === id);
     item.title = value;
     setTodo(temp);
   }
 
   function onDelete(id) {
-    const items = todo.filter((e) => e.id != id);
+    const items = todo.filter((e) => e.id !== id);
     setTodo(items);
   }
 
@@ -34,27 +34,30 @@ export default function TodoApp() {
   }
 
   return (
-    <div className="todoContainer">
-      <form className="todoCreateForm" onSubmit={(e) => handleSubmit(e)}>
-        <input
-          className="todoInput"
-          type="text"
-          onChange={(e) => handleChange(e)}
-          value={title}
-        ></input>
-        <button className="buttonCreate">Create ToDo</button>
-      </form>
-      <div className="todosContainer">
-        {todo.map((item) => {
-          return (
-            <ToDo
-              key={item.id}
-              item={item}
-              onUpdate={onUpdate}
-              onDelete={onDelete}
-            ></ToDo>
-          );
-        })}
+    <div>
+      <h1 className="title">TODO LIST</h1>
+      <div className="todoContainer">
+        <form className="todoCreateForm" onSubmit={(e) => handleSubmit(e)}>
+          <input
+            className="todoInput"
+            type="text"
+            onChange={(e) => handleChange(e)}
+            value={title}
+          ></input>
+          <button className="buttonCreate">Create ToDo</button>
+        </form>
+        <div className="todosContainer">
+          {todo.map((item) => {
+            return (
+              <ToDo
+                key={item.id}
+                item={item}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+              ></ToDo>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
